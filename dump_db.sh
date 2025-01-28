9f85737d4c3619fe72528d6d1393628ae66597f9
davidecontinanza <157005180+davidecontinanza@users.noreply.github.com>
2025-01-28 16:50:14 +0100

    Update dump_db.sh

--- dump_db.sh
+++ dump_db.sh
@@ -4,6 +4,6 @@ DOM=$(date +%d)
 backup_dir=./backup_db/$(date +'%Y-%m-%d').sql
 compressed=${backup_dir}.gz
 docker exec mysql sh -c 'exec mysqldump --all-databases --events -uroot -p"$MYSQL_ROOT_PASSWORD"' > ${backup_dir}
-rm -f -- ${compressed}
+rm -f ${compressed}
 gzip $backup_dir
 find ./backup_db -type f -mtime +28 -delete
