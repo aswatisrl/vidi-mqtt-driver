a93b922ab602bc49d4863a5889d852c318910479
davidecontinanza <157005180+davidecontinanza@users.noreply.github.com>
2025-01-28 16:43:27 +0100

    Update dump_db.sh

--- dump_db.sh
+++ dump_db.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+mkdir -p ./backup_db
 DOM=$(date +%d)
-backup_dir=/opt/nbiot-server/backup_db/$(date +'%Y-%m-%d').sql
+backup_dir=./backup_db/$(date +'%Y-%m-%d').sql
 docker exec mysql sh -c 'exec mysqldump --all-databases --events -uroot -p"$MYSQL_ROOT_PASSWORD"' > ${backup_dir}
 gzip $backup_dir
-find /opt/nbiot-server/backup_db -type f -mtime +28 -delete
-
-
-
+find ./backup_db -type f -mtime +28 -delete
