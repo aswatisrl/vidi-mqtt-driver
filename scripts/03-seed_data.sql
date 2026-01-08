906b312275153476751a1c9ad8aa793e0e3ddc69
davidecontinanza <157005180+davidecontinanza@users.noreply.github.com>
2026-01-08 17:44:24 +0100

    Update user insert statement with timestamp and null

--- scripts/03-seed_data.sql
+++ scripts/03-seed_data.sql
@@ -6,4 +6,4 @@ INSERT INTO `hardware_types` VALUES ('PRV_0','VIDI PRV Controller'),('TR2_0','VI
 
 INSERT INTO `settings` VALUES (1,'use_local_broker','1'),(2,'mqtt_broker','192.168.1.1'),(3,'mqtt_username','vidimqtt'),(4,'mqtt_password','vidimqtt'),(5,'mqtt_qos','1'),(6,'mqtt_topic_uplink','application/COAP/device/{{serial}}/uplink');
 
-INSERT INTO `users` VALUES (1,'admin','$2a$10$3mQ2F9owX3VEeOaiweyNpuNELcC7OaERlhIS4QUsk0UZlCXFww5tq',1,1,1,1,1);
+INSERT INTO `users` VALUES (1,'admin','$2a$10$3mQ2F9owX3VEeOaiweyNpuNELcC7OaERlhIS4QUsk0UZlCXFww5tq',null,CURRENT_TIMESTAMP(),1,1,1,1,1);
