665320987667baa580f7d1f2efe99b88982d5ef4
davidecontinanza <157005180+davidecontinanza@users.noreply.github.com>
2026-04-29 08:16:43 +0200

    Add new tables for applications and firmware versions

--- scripts/02-create_tables.sql
+++ scripts/02-create_tables.sql
@@ -21,6 +21,12 @@ CREATE TABLE IF NOT EXISTS `device_types` (
   PRIMARY KEY (`device_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE IF NOT EXISTS `applications` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE IF NOT EXISTS `devices` (
   `serial` varchar(8) NOT NULL,
   `insert_date` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -41,6 +47,7 @@ CREATE TABLE IF NOT EXISTS `devices` (
   `rsrp` int DEFAULT NULL,
   `snr` int DEFAULT NULL,
   `band` int DEFAULT NULL,
+  `battery_level` int DEFAULT NULL,
   `operator` varchar(255) DEFAULT NULL,
   `firmware` varchar(45) DEFAULT NULL,
   `tx_interval` int DEFAULT NULL,
@@ -51,7 +58,9 @@ CREATE TABLE IF NOT EXISTS `devices` (
   `timezone` varchar(45) DEFAULT NULL,
   `calendar` text,
   `app_id` int DEFAULT NULL,
-  PRIMARY KEY (`serial`)
+  PRIMARY KEY (`serial`),
+  KEY `fk_devices_application_idx` (`app_id`),
+  CONSTRAINT `fk_devices_application` FOREIGN KEY (`app_id`) REFERENCES `applications` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `downlink_queue` (
@@ -69,7 +78,8 @@ CREATE TABLE IF NOT EXISTS `downlink_queue` (
   `token` varchar(16) DEFAULT NULL,
   `response_payload` text,
   `response_code` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `idx_downlink_queue_serial` (`serial`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `hardware_types` (
@@ -111,14 +121,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255),
   `last_login` datetime DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `can_manage_users` int DEFAULT '0',
+  `is_admin` int DEFAULT '0',
   `can_read_devices` int DEFAULT '0',
   `can_write_devices` int DEFAULT '0',
   `can_send_downlink` int DEFAULT '0',
-  `can_manage_integrations` int DEFAULT '0',
+  `app_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  KEY `fk_users_application_idx` (`app_id`),
+  CONSTRAINT `fk_users_application` FOREIGN KEY (`app_id`) REFERENCES `applications` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `api_tokens` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -128,4 +140,41 @@ CREATE TABLE IF NOT EXISTS `api_tokens` (
   PRIMARY KEY (`id`),
   KEY `fk_users_token_idx` (`user_id`),
   CONSTRAINT `fk_users_token` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `firmware_versions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `version` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `requirement` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `upload_date` datetime DEFAULT NULL,
+  `checksum` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `serials` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `data` mediumblob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `mobile_devices` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `device_uuid` varchar(36) NOT NULL,
+  `app_name` varchar(255) DEFAULT NULL,
+  `app_version` varchar(45) DEFAULT NULL,
+  `device_name` varchar(255) DEFAULT NULL,
+  `last_access` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `device_uuid_UNIQUE` (`device_uuid`),
+  KEY `fk_mobile_devices_user_idx` (`user_id`),
+  CONSTRAINT `fk_mobile_devices_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `firmware_hwtype_compatibility` (
+  `hardware_type` varchar(16) NOT NULL,
+  `firmware_version` int NOT NULL,
+  PRIMARY KEY (`hardware_type`,`firmware_version`),
+  KEY `fk_firmware_version_idx` (`firmware_version`),
+  CONSTRAINT `fk_firmware_version` FOREIGN KEY (`firmware_version`) REFERENCES `firmware_versions` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_hardware_types` FOREIGN KEY (`hardware_type`) REFERENCES `hardware_types` (`hw_type`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
